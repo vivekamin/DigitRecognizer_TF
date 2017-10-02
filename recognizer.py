9a1816b905dc8f05748c0c251916f9c3ada130d3
vivekamin <vivek@yodaplus.com>
2017-10-01 19:12:21 -0700

    Cross Entropy,training step defined, Initialized Global Vars

--- recognizer.py
+++ recognizer.py
@@ -11,3 +11,9 @@ y = tf.nn.softmax(tf.matmul(x,W) + b)
 
 y_ = tf.placeholder(tf.float32, [None, 10])
 
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
