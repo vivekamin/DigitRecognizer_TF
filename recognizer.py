eecd593af690cad6a25c1fc20403007d65189765
vivekamin <vivek@yodaplus.com>
2017-10-01 19:04:05 -0700

    Weights, Bias Initialized + Softmax Defined

--- recognizer.py
+++ recognizer.py
@@ -3,5 +3,11 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
+x = tf.placeholder(tf.float32 , [None, 784])  #input: image vectors
+W = tf.Variable(tf.Zeros([784,10]))
+b = tf.Variable(tf.Zeros([10]))
 
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+
+y_ = tf.placeholder(tf.float32, [None, 10])
 
