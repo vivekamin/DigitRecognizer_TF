3d43099f82d0e548e29e033b610a7e2d3284447a
vivekamin <vivek@yodaplus.com>
2017-10-01 20:13:05 -0700

    Training step and Evaluation Added

--- recognizer.py
+++ recognizer.py
@@ -3,9 +3,9 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-x = tf.placeholder(tf.float32 , [None, 784])  #input: image vectors
-W = tf.Variable(tf.Zeros([784,10]))
-b = tf.Variable(tf.Zeros([10]))
+x = tf.placeholder(tf.float32 , [None, 784])  # input: image vectors
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
 
 y = tf.nn.softmax(tf.matmul(x,W) + b)
 
@@ -17,3 +17,14 @@ train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
+
+for _ in range(1):
+    batch_xs , batch_ys = mnist.train.next_batch(100)
+    print(sess.run(train_step,feed_dict= {x: batch_xs,y_:batch_ys}))
+
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+print (batch_xs.shape)
